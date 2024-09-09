89d250d75e34749519fcaa81e62799ea6963d074
Artem Shavriev <artem-shavriev@yandex.ru>
2024-09-09 08:47:57 +0300

    ФЗ 6. Добавить тесты дя новых методов InMemoryHistoryManager.

--- test/service/InMemoryHistoryManagerTest.java
+++ test/service/InMemoryHistoryManagerTest.java
@@ -18,13 +18,13 @@ public class InMemoryHistoryManagerTest {
 
     @Test
     void shouldAddTaskToHistoryList() {
-        Task task = new Task("Уборка", "Собрать и вынести мусор",  TaskStatus.NEW);
+        Task task = new Task(1,"Уборка", "Собрать и вынести мусор",  TaskStatus.NEW);
         historyManager.add(task);
 
-        Epic epic = new Epic(1,"Сделать ремонт", "Покрасить стены на балконе");
+        Epic epic = new Epic(2,"Сделать ремонт", "Покрасить стены на балконе");
         historyManager.add(epic);
 
-        Subtask subtask = new Subtask(10, 1,"Купить шпатель",
+        Subtask subtask = new Subtask(10, 2,"Купить шпатель",
                 "Выбрать в магазине шпатель и купить", TaskStatus.NEW);
         historyManager.add(subtask);
 
@@ -40,9 +40,51 @@ public class InMemoryHistoryManagerTest {
 
     @Test
     void shouldGetHistory() {
-        Task task = new Task("Уборка", "Собрать и вынести мусор",  TaskStatus.NEW);
+        Task task = new Task(1,"Уборка", "Собрать и вынести мусор",  TaskStatus.NEW);
         historyManager.add(task);
-        assertNotNull(historyManager.getHistory().get(0), "История не получгена.");
+        assertNotNull(historyManager.getHistory().get(0), "История не получена.");
     }
 
+    @Test
+    void shouldNotRepeatElementInHistory() {
+        Task task = new Task(1,"Уборка", "Собрать и вынести мусор",  TaskStatus.NEW);
+        historyManager.add(task);
+        historyManager.add(task);
+
+        Epic epic = new Epic(2,"Сделать ремонт", "Покрасить стены на балконе");
+        historyManager.add(epic);
+        historyManager.add(epic);
+
+        Subtask subtask = new Subtask(10, 2,"Купить шпатель",
+                "Выбрать в магазине шпатель и купить", TaskStatus.NEW);
+        historyManager.add(subtask);
+        historyManager.add(subtask);
+
+        Subtask subtask2 = new Subtask(11, 2,"Купить краску",
+                "Выбрать краску и купить", TaskStatus.DONE);
+        historyManager.add(subtask2);
+
+        assertNotEquals(historyManager.getHistory().get(0), historyManager.getHistory().get(1),
+                "Задачи в истории не повторяются.");
+        assertNotEquals(historyManager.getHistory().get(1), historyManager.getHistory().get(2),
+                "Эпики в истории не повторяются.");
+        assertNotEquals(historyManager.getHistory().get(2), historyManager.getHistory().get(3),
+                "Эпики в истории не повторяются.");
+
+    }
+
+    @Test
+    public void shouldRemoveFromHistory() {
+        Task task = new Task(1, "Уборка", "Собрать и вынести мусор", TaskStatus.NEW);
+        historyManager.add(task);
+
+        Epic epic = new Epic(2, "Сделать ремонт", "Покрасить стены на балконе");
+        historyManager.add(epic);
+
+        historyManager.remove(1);
+        assertNotEquals(historyManager.getHistory().get(0), task, "Задача id:1 не удалена");
+        historyManager.remove(2);
+        assertNotEquals(historyManager.getHistory().get(0), task, "Задача id:2 не удалена");
+
+    }
 }
